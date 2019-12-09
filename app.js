cd074d2fe3b9e92b8125a6eb26d5506c7354a90f
abdelsalamshahlol <abdulsalamshahlol@gmail.com>
2019-12-09 14:23:31 +0100

    progress

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // TODO
+
+// var MyTag = () => (<h1>Test</h1>);
+var GroceryList = ()=>(
+	<ul>
+		<li>Bread</li>
+		<li>Milk</li>
+	</ul>
+);
+
+var App = () => (
+	<div>
+		<GroceryList/>
+	</div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
