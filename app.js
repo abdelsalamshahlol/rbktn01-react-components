2810a1b88ae09ac5cc34213335427957196f54f4
abdelsalamshahlol <abdulsalamshahlol@gmail.com>
2019-12-09 15:21:30 +0100

    progress

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
 // TODO
-var Item = (props)=> (<ul><li>{props.arr[0]}</li><li>{props.arr[1]}</li></ul>)
+class Item extends React.Component {
 
-var GroceryList = ()=>(
-		<Item arr={['Bread', 'Milk']}  />
+	 constructor(props){
+	 	super(props)
+	 	this.state = {
+	 		isBold: false
+	 	}; 
+	 }
+
+	 onHover(){
+	 	this.setState({
+	 		isBold: !this.state.isBold
+	 	});
+	 }
+
+
+	 render(){
+	 	let boldStyle = {
+	 		'fontWeight' : this.state.isBold ? 'bold' : 'unset'
+	 	}
+	 	return (<li style={boldStyle} onMouseOver={this.onHover.bind(this)} onMouseOut={this.onHover.bind(this)}>{this.props.arr[0]}</li>)
+	 }
 		
-	
-);
+
+}  
+
+class GroceryList extends React.Component{
+	constructor(props){
+		super(props)
+
+	}
+	render(){
+		return <Item arr={['Bread', 'Milk']}  />
+	}
+
+
+
+}
 
 var App = () => (
 	<div>
